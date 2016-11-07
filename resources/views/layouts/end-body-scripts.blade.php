7aedcbe3c2cdb76e96a381c76b105c1cf941ed82
Brent Schuddinck <contact@brentschuddinck.be>
2016-11-07 23:57:04 +0100

    Added bootbox with confirm dialog to delete user account

--- resources/views/layouts/end-body-scripts.blade.php
+++ resources/views/layouts/end-body-scripts.blade.php
@@ -1,2 +1,4 @@
-<script type="text/javascript" src="js/vendor.js"></script>
-<script type="text/javascript" src="js/app.js"></script>
+<script type="text/javascript" src="/js/vendor.js"></script>
+<script type="text/javascript" src="/js/app.js"></script>
+<script type="text/javascript" src="/js/bootbox.min.js"></script>
+@yield('custom-scripts')
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -47,7 +47,7 @@
                             <td class="hidden-xs hidden-sm"><a href="mailto:{{$user->email}}">{{$user->email}}</a></td>
                             <td class="hidden-xs">{{ $user->admin == 1 ? 'Admin' : 'Standard user' }}</td>
                             <td><a href="/users/{{$user->id}}/edit" class="btn-sm btn-success"><i class="icon fa fa-pencil" aria-hidden="true"></i><span class="hidden-xs">&nbsp;Edit user</span></a></td>
-                            <td>@if($user->id != 1) <a href="/users/{{$user->id}}/delete" class="btn-sm btn-danger"><i class="icon fa fa-trash" aria-hidden="true"></i><span class="hidden-xs">&nbsp;Delete user</span></a> @endif</td>
+                            <td>@if($user->id != 1) <a href="#" class="btn-sm btn-danger btn-delete" data-id="{{$user->id}}" data-name="{{$user->name}}"><i class="icon fa fa-trash" aria-hidden="true"></i><span class="hidden-xs">&nbsp;Delete user</span></a> @endif</td>
                         </tr>
                     @endforeach
                     </tbody>
@@ -55,4 +55,32 @@
             </div>
         </div>
     </div>
+@stop
+
+@section('custom-scripts')
+        <!-- Delete confirm bootbox -->
+    <script>
+        $(document).on("click", ".btn-delete", function(e) {
+            var id_value = $(this).attr('data-id');
+            var name_value = $(this).attr('data-name');
+            bootbox.confirm({
+                message: "Are you sure you want to delete user " + name_value + "? All data and settings of this user will be deleted. This action is irreversible.",
+                buttons: {
+                    confirm: {
+                        label: 'Yes, delete this user',
+                        className: 'btn-danger'
+                    },
+                    cancel: {
+                        label: 'Cancel',
+                        className: 'btn-default'
+                    }
+                },
+                callback: function (result) {
+                    if(result){
+                        window.location = "/users/" + id_value + "/delete";
+                    }
+                }
+            });
+        });
+    </script>
 @stop
