b953abf4544eeb115bf44a7cb4cc748e96063a5c
Arno De Decker <arnodedecker@telenet.be>
2016-11-17 17:17:11 +0100

    Updated blocks

--- resources/views/layouts/end-body-scripts.blade.php
+++ resources/views/layouts/end-body-scripts.blade.php
@@ -1,5 +1,4 @@
 <script type="text/javascript" src="/js/vendor.js"></script>
 <script type="text/javascript" src="/js/app.js"></script>
 <script type="text/javascript" src="/js/bootstrap.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js">
 @yield('custom-scripts')
